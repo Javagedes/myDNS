da9e00973b8510566df2c3abfd9ddecb1aaeb021
Javagedes <joey.vagedes@gmail.com>
2021-08-18 12:48:51 -0700

    React can call APIs to pull data to frontend

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    entries: []
+  }
+
+  componentDidMount() {
+    console.log("We called this function")
+    axios.get('https://localhost:5001/api/DNSEntry').then((response) => {
+      this.setState({
+        entries: response.data
+      })
+      console.log(response.data)
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h3>List of DNS Entries</h3>
+          <ul>
+            {this.state.entries.map((entry:any) => (
+              <li key="{entry.id}" >{ entry.hostName } - {entry.type} - {entry.value} </li>
+            ))}
+          </ul>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
