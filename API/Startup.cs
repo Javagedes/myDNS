871bd7af7d28372371205bcfc40bbd329304537b
Javagedes <joey.vagedes@gmail.com>
2021-08-18 09:04:46 -0700

    Basic API calling now working

--- API/Startup.cs
+++ API/Startup.cs
@@ -12,7 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-//using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -32,12 +32,10 @@ namespace API
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
-            /* Not sure if this is needed?
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
-            */
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
