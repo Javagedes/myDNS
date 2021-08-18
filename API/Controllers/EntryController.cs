f98ec51dc37e95ad4ea424d7237050f33088a49c
Javagedes <joey.vagedes@gmail.com>
2021-08-18 09:25:20 -0700

    react-app loaded incorrectly as Javascript. Removed and replaced with Typescript

--- API/Controllers/EntryController.cs
+++ API/Controllers/EntryController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers {
+    
     [Route("api/DNSEntry")]
     [ApiController]
     public class EntryController : ControllerBase
---
+++ README.md
@@ -0,0 +1,4 @@
+This project simulates a simplified DNS as a webapp
+
+This project uses React (Typescript) for the front end
+This project uses C# (.NET) and the Entity Framework Core to connect to a SQLite Database
