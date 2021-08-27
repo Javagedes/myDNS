a6dd3732a5ca23247f03fedd17e690778b2d7d08
Javagedes <joey.vagedes@gmail.com>
2021-08-26 17:14:31 -0700

    Pushing API changes

--- API/Controllers/EditRecordController.cs
+++ API/Controllers/EditRecordController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Database;
 using Domain;
@@ -8,6 +9,9 @@ using Microsoft.EntityFrameworkCore;
 using System.Net.Http;
 using System.Net;
 using Microsoft.Extensions.Caching.Memory;
+using System.Web;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
 
 namespace API.Controllers {
     
@@ -16,47 +20,84 @@ namespace API.Controllers {
     public class EditRecordController : ControllerBase
     {
         private readonly DataContext _Context;
-        private Cache _Cache;
+        private IMemoryCache _Cache;
+
         public EditRecordController(DataContext context)
         {
             _Context = context;
-            _Cache = new Cache(_Context);
+            _Cache = new MemoryCache( new MemoryCacheOptions()
+            {
+                SizeLimit = 1024
+            });
+
+            foreach (DNSEntry entry in _Context.Entries.ToList())
+            {
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSize(1)
+                    .SetAbsoluteExpiration(System.TimeSpan.FromSeconds(entry.TTL));
+                     
+                    _Cache.Set(entry.HostName+entry.Type, entry, cacheEntryOptions);
+            }
         }
 
-        //A Request to get all entries
+        //A Request to get an entry
         [HttpGet]
-        public async Task<ActionResult<DNSEntry>> Get([FromBody] DNSEntryLookupItem item)
+        public async Task<ActionResult<DNSEntry>> Get([FromBody] LookupKey lookupKey)
         {
-            DNSEntry entry;
-            entry = await _Cache.Get(_Context, item);
-            
-            if(entry == default)
+            DNSEntry record;
+
+            if(!_Cache.TryGetValue(lookupKey.ToString(), out record))
             {
-                System.Console.WriteLine("We are here!");
-                return new EmptyResult();
+                System.Console.WriteLine("Key was not in cache");
+                
+                try 
+                {
+                    record = await _Context.Entries
+                        .SingleOrDefaultAsync(table => table.HostName == lookupKey.HostName && table.Type == lookupKey.Type);
+                }
+                catch
+                {
+                    //TODO: Will throw an exception if it finds more than one matching record.
+                    // Need to handle that exception here.
+                    record = null;
+                }     
+
+                if (record != null) 
+                {
+                    var cacheEntryOptions = new MemoryCacheEntryOptions()
+                        .SetSize(1)
+                        .SetAbsoluteExpiration(System.TimeSpan.FromSeconds(record.TTL));
+
+                    _Cache.Set(lookupKey, record, cacheEntryOptions);
+                }
             }
-            
-            return Ok(entry);
+
+            if(record != null)
+            {
+                return Ok(record);
+            }
+            else
+            {
+                return new EmptyResult();
+            }  
         }
 
         //A Request to add an entry
         [HttpPost]
         public async Task<HttpResponseMessage> Post([FromBody] DNSEntry entry)
         {
-            //TODO: _Context.SaveChanges();
-            System.Console.Write(entry.HostName);
-            HttpResponseMessage returnMessage=new HttpResponseMessage();
-            _Context.Entries.Add(entry);
-
+            HttpResponseMessage returnMessage = new HttpResponseMessage();
+            
             try {
-                
+
+                _Context.Entries.Add(entry);
                 _Context.SaveChanges();
                 string message = ($"Entry Created - {entry.HostName}");
                 returnMessage = new HttpResponseMessage(HttpStatusCode.Created);
                 returnMessage.RequestMessage = new HttpRequestMessage(HttpMethod.Post, message);
             }
             //TODO: Look into other possible errors
-            catch (DbUpdateException)
+            catch
             {
                 returnMessage = new HttpResponseMessage(HttpStatusCode.ExpectationFailed);
                 returnMessage.RequestMessage = new HttpRequestMessage(HttpMethod.Post, "Entry Already Exists");
@@ -66,106 +107,44 @@ namespace API.Controllers {
         }
 
         [HttpDelete]
-        public async Task<HttpResponseMessage> Delete([FromBody] DNSEntry entry)
+        public async Task<HttpResponseMessage> Delete([FromBody] LookupKey lookupKey)
         {
             HttpResponseMessage returnMessage=new HttpResponseMessage();
-            _Context.Entries.Remove(entry);
+            
+            _Cache.Remove(lookupKey.ToString());
 
-            try {
-                 
-                _Context.SaveChanges();
-                string message = ($"Entry Removed - {entry.HostName}");
-                returnMessage = new HttpResponseMessage(HttpStatusCode.OK);
-            }
-            catch (DbUpdateException)
+            DNSEntry record = await _Context.Entries
+                        .SingleOrDefaultAsync(table => table.HostName == lookupKey.HostName && table.Type == lookupKey.Type);
+
+            if (record == null)
             {
                 returnMessage = new HttpResponseMessage(HttpStatusCode.ExpectationFailed);
-                returnMessage.RequestMessage = new HttpRequestMessage(HttpMethod.Post, "Entry Already Exists");
+                returnMessage.RequestMessage = new HttpRequestMessage(HttpMethod.Post, "Entry Does not Exist");
+                return await Task.FromResult(returnMessage);
             }
-
+            
+            _Context.Entries.Remove(record);
+             _Context.SaveChanges();
+            string message = ($"{record.HostName} Removed.");
+            returnMessage = new HttpResponseMessage(HttpStatusCode.OK);
             return await Task.FromResult(returnMessage);
         }
-    }
+    } 
 
-    public class Cache
-    {
-        private MemoryCache _Cache; 
-
-        public Cache(DataContext context)
-        {
-            _Cache = new MemoryCache( new MemoryCacheOptions()
-            {
-                SizeLimit = 1024
-            });
-
-            foreach (DNSEntry entry in context.Entries.ToList())
-            {
-                var cacheEntryOptions = new MemoryCacheEntryOptions()
-                    .SetSize(1)
-                    .SetAbsoluteExpiration(System.TimeSpan.FromSeconds(entry.TTL));
-                     
-                     DNSEntryLookupItem key = new DNSEntryLookupItem(entry.HostName, entry.Type);
-                    _Cache.Set(key, entry, cacheEntryOptions);
-            }
-        }
-
-
-        /*
-            This function queries the Entries database for a DNSEntry that has the correct Hostname and correct 
-                entry value type (A, AAAA, etc);
-            This function will return the matching DNSEntry, if found. Currently, if it is not found, or 
-                multiple entries are found, it will return a null value. 
-            When using this function, you must check for null!
-        */
-        public async Task<DNSEntry> Get(DataContext context, DNSEntryLookupItem item)
-        {
-            DNSEntry cacheEntry;
-
-            if(!_Cache.TryGetValue(item, out cacheEntry))
-            {   
-                System.Console.WriteLine("Could not find the item in cache");
-                try 
-                {
-                    cacheEntry = await context.Entries.SingleOrDefaultAsync(table => table.HostName == item.HostName && table.Type == item.Type);
-                }
-                catch
-                {
-                    //TODO: Will throw an exception if it finds more than one matching record.
-                    // Need to handle that exception here.
-                    cacheEntry = null;
-                }
-                
-                if (cacheEntry != null) 
-                {
-                    var cacheEntryOptions = new MemoryCacheEntryOptions()
-                    .SetSize(1)
-                    .SetAbsoluteExpiration(System.TimeSpan.FromSeconds(cacheEntry.TTL));
-                    _Cache.Set(cacheEntry.HostName, cacheEntry, cacheEntryOptions);
-                }   
-            }
-            return cacheEntry;
-        }
-
-        public void Remove(string hostName)
-        {
-
-        }
-
-        public void Add(DNSEntry item) 
-        {
-
-        }
-    }
-
-    public class DNSEntryLookupItem
+    public class LookupKey
     {   
         public string HostName  { get; set; }
         public string Type      { get; set; }
 
-        public DNSEntryLookupItem(string HostName, string Type)
+        public LookupKey(string HostName, string Type)
         {
             this.HostName = HostName;
             this.Type = Type;
+        }
+
+        public override string ToString()
+        {
+            return HostName+Type;
         }
     }
 }
